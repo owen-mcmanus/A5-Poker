6780a3fa4d6d449460fb42b0e8d1c1fdf90fa94d
RmaUriel <uhernandez11404@gmail.com>
2025-06-12 19:22:25 -0700

    loop

--- src/main/java/T4ASouthPanelNanny.java
+++ src/main/java/T4ASouthPanelNanny.java
@@ -35,7 +35,7 @@ public class T4ASouthPanelNanny {
                     StringBuilder builder = new StringBuilder();
                     storiesQueue.forEach(story -> builder.append(story).append("\n"));
                     upcomingArea.setText(builder.toString());
-//                    logger.info("Updated upcoming stories with {} items.", storiesQueue.size());
+
                 }
 
                 case "completedStory" -> {
@@ -46,18 +46,15 @@ public class T4ASouthPanelNanny {
                         builder.append(story[0]).append(": ").append(story[1]).append("\n");
                     }
                     completedArea.setText(builder.toString());
-//                    logger.info("Completed stories updated with {} entries.", storiesList.size());
+
                 }
 
                 case "activeStory" -> {
                     String activeStory = (String) evt.getNewValue();
                     activeArea.setText(activeStory);
-//                    logger.info("Active story set to: {}", activeStory);
+
                 }
 
-                default -> {
-                    logger.debug("Unhandled property change: {}", prop);
-                }
             }
         } catch (ClassCastException e) {
             logger.error("Property '{}' value could not be cast correctly", prop, e);
